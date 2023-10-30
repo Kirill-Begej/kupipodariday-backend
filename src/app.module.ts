801654e24ae1ea272c10fbc0a38d01b6b6ff2a9d
Kirill Begey <kirill.begey@mail.ru>
2023-10-30 11:25:45 +0300

    feat: Создание проекта

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import devConfig from './config/dev.config';
-import { configValidationSchema } from './config/config.validation';
-import { TypeOrmConfigService } from './config/orm.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { OffersModule } from './offers/offers.module';
-import { WishlistsModule } from './wishlists/wishlists.module';
-import { WishesModule } from './wishes/wishes.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: ['.env.dev'],
-      load: [devConfig],
-      isGlobal: true,
-      validationSchema: configValidationSchema,
-    }),
-    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
-    UsersModule,
-    WishesModule,
-    WishlistsModule,
-    OffersModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-  const PORT = configService.get('port');
-  await app.listen(PORT);
+  await app.listen(3000);
 }
 bootstrap();
